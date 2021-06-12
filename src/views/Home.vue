2907348e6cda911e114211455f2ee66e0aefc24e
jason06286 <dj4871114@gmail.com>
2021-06-13 02:29:15 +0800

    update VueCli

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <router-link to="/user/cart" class="btn btn-success mx-3"
       >前台介面</router-link
     >
-    <router-link to="/login" class="btn btn-danger">後台介面</router-link>
+    <router-link to="/dashboard" class="btn btn-danger">後台介面</router-link>
   </div>
 </template>
 
